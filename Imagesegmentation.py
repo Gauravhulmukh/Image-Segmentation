42b258164f08a122842bc87366c71eedee36793a
Gaurav Hulmukh <37579974+Gauravhulmukh@users.noreply.github.com>
2018-12-22 15:52:34 +0530

    Update and rename Image_segmentation.py to Imagesegmentation.py

--- Imagesegmentation.py
+++ Imagesegmentation.py
@@ -1,4 +1,5 @@
-import cv2 
+import cv2
+
 img = cv2.imread('C:\\Users\\gauravhulmukh\\Desktop\\Matrix-Test\\003.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.bilateralFilter(gray, 11, 17, 17)
@@ -12,13 +13,14 @@ cv2.waitKey(0)
 i=0
 cnts,heir= cv2.findContours(closed.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
 for c in cnts: 	
-#peri = cv2.arcLength(c, True) 	
-#approx = cv2.approxPolyDP(c, 0.02 * peri, True) 
-#cv2.drawContours(img, [approx], -1, (0, 255, 0), 2)	
-x,y,w,h =cv2.boundingRect(c)	
-i=i+1;	
-newImage=img[y:y+h,x:x+w]	
-cv2.imwrite(str(i)+'.jpg',newImage) 
+    #peri = cv2.arcLength(c, True) 	
+    #approx = cv2.approxPolyDP(c, 0.02 * peri, True) 
+    #cv2.drawContours(img, [approx], -1, (0, 255, 0), 2)	
+    x,y,w,h =cv2.boundingRect(c)	
+    i=i+1;	
+    newImage=img[y:y+h,x:x+w]	
+    cv2.imwrite(str(i)+'.jpg',newImage) 
+    
 cv2.imshow('dst_rt', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
